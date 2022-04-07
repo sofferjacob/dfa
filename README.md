781a99fb68fa090aa6be4047f66b3fc9c934b23e
Jacobo Soffer <sofferjacob@gmail.com>
2022-04-07 18:36:58 -0500

    Add states

--- README.md
+++ README.md
@@ -23,17 +23,17 @@ Los estados de aeptación son los siguientes:
 * c_par
 
 ## Tabla de Transiciones
-| **Estado/Input** | **+\|-** | **dig** | **+\|-\|*\|/** | **.** | **e\|E** | **alpha** | **=**  | **(** | **)** | **_** |
-|------------------|----------|---------|----------------|-------|----------|-----------|--------|-------|-------|-------|
-| **start**        | n_sign   | int     | F              | F     | F        | var       | F      | c_par | F     | F     |
-| **n_sign**       | F        | int     | F              | F     | F        | F         | F      | F     | F     | F     |
-| **int**          | N/A      | int     | op             | real  | F        | F         | F      | o_par | c_par | F     |
-| **op**           | n_sign   | int     | F              | F     | F        | F         | F      | o_par | F     | F     |
-| **var**          | F        | var     | F              | F     | N/A      | var       | assign | F     | F     | var   |
-| **assign**       | n_sign   | int     | F              | F     | N/A      | var       | F      | o_par | F     | F     |
-| **o_par**        | n_sign   | int     | F              | F     | F        | var       | F      | F     | F     | F     |
-| **c_par**        | N/A      | int     | op             | F     | F        | var       | F      | o_par | c_par | F     |
-| **real**         | N/A      | real    | op             | F     | realexp  | F         | F      | F     | c_par | F     |
-| **realexp**      | exp_sign | exp_dig | F              | F     | F        | F         | F      | F     | F     | F     |
-| **exp_sign**     | F        | exp_dig | F              | F     | F        | F         | F      | F     | F     | F     |
-| **exp_dig**      | N/A      | F       | op             | F     | F        | F         | F      | o_par | c_par | F     |
+| **Estado/Input** | **+\|-** | **dig** | **+\|-\|*\|/** | **.** | **e\|E** | **alpha** | **=**  | **(** | **)** | **_** | **" "** |
+|------------------|----------|---------|----------------|-------|----------|-----------|--------|-------|-------|-------|---------|
+| **start**        | n_sign   | int     | F              | F     | F        | var       | F      | o_par | F     | F     | start   |
+| **n_sign**       | F        | int     | F              | F     | F        | F         | F      | o_par | F     | F     | n_sign  |
+| **int**          | N/A      | int     | op             | real  | F        | F         | F      | o_par | c_par | F     | F       |
+| **op**           | n_sign   | int     | F              | F     | F        | F         | F      | o_par | F     | F     | op      |
+| **var**          | F        | var     | op             | F     | N/A      | var       | assign | o_par | c_par | var   | var     |
+| **assign**       | n_sign   | int     | F              | F     | N/A      | var       | F      | o_par | F     | F     | assign  |
+| **o_par**        | n_sign   | int     | F              | F     | F        | var       | F      | F     | F     | F     | o_par   |
+| **c_par**        | N/A      | int     | op             | F     | F        | var       | F      | o_par | c_par | F     | c_par   |
+| **real**         | N/A      | real    | op             | F     | realexp  | F         | F      | F     | c_par | F     | F       |
+| **realexp**      | exp_sign | exp_dig | F              | F     | F        | F         | F      | F     | F     | F     | F       |
+| **exp_sign**     | F        | exp_dig | F              | F     | F        | F         | F      | F     | F     | F     | F       |
+| **exp_dig**      | N/A      | F       | op             | F     | F        | F         | F      | o_par | c_par | F     | F       |
--- main.rkt
+++ main.rkt
@@ -1,7 +1,5 @@
 #lang racket
 
-; TODO: Add spaces
-
 (define (sign? c) (member c '(#\+ #\-)))
 (define (operator? c) (member c '(#\+ #\- #\* #\/)))
 
@@ -13,6 +11,8 @@
     cond
         [(sign? c) [values n_sign #t "signo"]]
         [(char-numeric? c) [values int #t "entero"]]
+        [(char-alphabetic? c) [values var #t "variable"]]
+        [(eq? c #\space) [values start #f "espacio"]]
         [(eq? c #\() [values o_par #t "parentesis abierto"]]
         [else (error (string-append [string c] " Error"))]
 ))
@@ -21,17 +21,145 @@
 ;     if (sign? c) [values n_sign #t "signo"] (if (char-numeric? c) (values int #t "entero")  (error (string-append [string c] " Error")))
 ; ))
 
-(define (n_sign c) 
-    (if (char-numeric? c) (values int #t "entero") (error (string-append [string c] " Error")))
+(define (n_sign c)
+    "Estado de signo de número"
+    ;(if (char-numeric? c) (values int #t "entero") (error (string-append [string c] " Error")))
+    (
+    cond
+        [(char-numeric? c) [values int #t "entero"]]
+        [(eq? c #\space) [values n_sign #f "signo"]]
+        [(eq? c #\() [values o_par #t "parentesis abierto"]]
+        [else (error (string-append [string c] " Error"))]
+)
 )
 
-(define (o_par c) (
-    if (char-numeric? c) (values int #t "entero") (if (sign? c) [values n_sign #t "signo"] [error (string-append [string c] " Error")])
-))
+(define (o_par c)
+    "Estado de parentesis abierto"
+    ;if (char-numeric? c) (values int #t "entero") (if (sign? c) [values n_sign #t "signo"] [error (string-append [string c] " Error")])
+    (
+    cond
+        [(sign? c) [values n_sign #t "signo"]]
+        [(char-numeric? c) [values int #t "entero"]]
+        [(eq? c #\space) [values o_par #f "parentesis abierto"]]
+        [(char-alphabetic? c) [values var #t "variable"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
 
-(define (int c) (if (char-numeric? c) (values int #f "entero") (if (operator? c) [values op #t "operator"] (error (string-append [string c] " Error")))))
+(define (int c)
+    "Estado número entero"
+    ;if (char-numeric? c) (values int #t "entero") (if (sign? c) [values n_sign #t "signo"] [error (string-append [string c] " Error")])
+    (
+    cond
+        [(operator? c) [values op #t "operator"]]
+        [(char-numeric? c) [values int #f "entero"]]
+        [(eq? c #\.) [values real #f "real"]]
+        [(eq? c #\() [values o_par #t "parentesis abierto"]]
+        [(eq? c #\)) [values c_par #t "parentesis cerrado"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
 
-(define (op c) (if (char-numeric? c) [values int #t "entero"] (if (operator? c) [values n_sign #t "signo"] [error (string-append [string c] " Error")])))
+;(define (int c) (if (char-numeric? c) (values int #f "entero") (if (operator? c) [values op #t "operator"] (error (string-append [string c] " Error")))))
+
+;(define (op c) (if (char-numeric? c) [values int #t "entero"] (if (operator? c) [values n_sign #t "signo"] [error (string-append [string c] " Error")])))
+
+(define (op c)
+    "Estado operador"
+    (
+    cond
+        [(sign? c) [values n_sign #t "signo"]]
+        [(char-numeric? c) [values int #t "entero"]]
+        [(eq? c #\space) [values op #f "operador"]]
+        [(eq? c #\() [values o_par #t "parentesis abierto"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
+
+(define (var c)
+    "Estado variable"
+    (
+    cond
+        [(operator? c) [values op #t "operador"]]
+        [(char-numeric? c) [values var #f "variable"]]
+        [(char-alphabetic? c) [values var #f "variable"]]
+        [(eq? c #\_) [values var #f "variable"]]
+        [(eq? c #\=) [values assign #t "asignacion"]]
+        [(eq? c #\space) [values var #f "variable"]]
+        [(eq? c #\() [values o_par #t "parentesis abierto"]]
+        [(eq? c #\)) [values c_par #t "parentesis cerrado"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
+
+(define (assign c)
+    "Estado de asignación"
+    (
+    cond
+        [(sign? c) [values n_sign #t "signo"]]
+        [(char-numeric? c) [values int #t "entero"]]
+        [(char-alphabetic? c) [values var #t "variable"]]
+        [(eq? c #\space) [values assign #f "asignacion"]]
+        [(eq? c #\() [values o_par #t "parentesis abierto"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
+
+(define (c_par c)
+    "Estado parentesis cerrado"
+    (
+    cond
+        [(operator? c) [values op #t "operador"]]
+        [(char-numeric? c) [values int #t "entero"]]
+        [(char-alphabetic? c) [values var #f "variable"]]
+        [(eq? c #\space) [values c_par #f "parentesis cerrado"]]
+        [(eq? c #\() [values o_par #t "parentesis abierto"]]
+        [(eq? c #\)) [values c_par #t "parentesis cerrado"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
+
+(define (real c)
+    "Estado número real"
+    (
+    cond
+        [(operator? c) [values op #t "operador"]]
+        [(char-numeric? c) [values real #f "real"]]
+        [(or (eq? c #\e) (eq? c #\E)) [values realexp #f "real"]]
+        [(eq? c #\)) [values c_par #t "parentesis cerrado"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
+
+(define (realexp c)
+    "Estado número real, con extensión de exponente"
+    (
+    cond
+        [(sign? c) [values exp_sign #f "real"]]
+        [(char-numeric? c) [values exp_dig #f "real"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
+
+(define (exp_sign c)
+    "Estado número real, con extensión de exponente con signo"
+    (
+    cond
+        [(char-numeric? c) [values exp_dig #f "real"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
+
+(define (exp_dig c)
+    "Estado número real, con extensión de exponente y digito"
+    (
+    cond
+        [(operator? c) [values op #t "operador"]]
+        [(eq? c #\() [values o_par #t "parentesis abierto"]]
+        [(eq? c #\)) [values c_par #t "parentesis cerrado"]]
+        [else (error (string-append [string c] " Error"))]
+)   
+)
 
 (define accepted-states '(int))
 
@@ -52,9 +180,7 @@
                     (loop next (cdr chars) 
                         (if [> (string-length buffer) 0]
                             (string-append res buffer " \n" strstate " " [string(car chars)] " \n")
-                            ;(cons (string-append strstate " " (string[car chars])) (cons [string-append curr-state " " buffer] res))
                             (string-append res " \n" strstate " " [string(car chars)])
-                            ;(cons [string-append strstate " " (string[car chars])] res)
                         ) "" strstate
                     )
                     (loop next (cdr chars) res (string-append buffer [string(car chars)]) strstate)
